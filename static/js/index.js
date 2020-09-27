89243331bf229b77d589472004a9b4c01341d90b
Jovani Pink <jovanirosado@hotmail.com>
2020-09-27 13:18:28 -0400

    Fixed the jQuery to reload the page.

--- static/js/index.js
+++ static/js/index.js
@@ -1,37 +1,41 @@
-// Variables
-scrapeButton = $("#scrape");
-scrapeSpinner = $("#scrape-spinner");
+$(document).ready(function () {
+  // Variables
+  scrapeButton = $("#scrape");
+  scrapeSpinner = $("button#scrape span");
 
-// Helper function to get the status of the task
-function updateProgress(status_url) {
-  // send GET request to status URL
-  $.getJSON(status_url, function (data) {
-    if (data["state"] == "SUCCESS") {
-      scrapeButton.text("Updated");
-      scrapeSpinner.toggleClass("d-none");
-    } else {
-      // Rerun in 2 seconds
-      setTimeout(function () {
+  // Helper function to get the status of the task
+  function updateProgress(status_url) {
+    // send GET request to status URL
+    $.getJSON(status_url, function (data) {
+      if (data["state"] == "SUCCESS") {
+        scrapeButton.text("Updated");
+        scrapeSpinner.toggleClass("d-none");
+        location.reload().delay(800);
+      } else {
+        // Rerun in 2 seconds
+        setTimeout(function () {
+          updateProgress(status_url);
+        }, 2000);
+      }
+    });
+  }
+
+  scrapeButton.on("click", function (e) {
+    e.preventDefault();
+
+    $(this).text("Loading");
+    scrapeSpinner.toggleClass("d-none");
+
+    $.ajax({
+      type: "POST",
+      url: "/longtask",
+      success: function (data, status, request) {
+        status_url = request.getResponseHeader("Location");
         updateProgress(status_url);
-      }, 2000);
-    }
-  });
-}
-
-$("#scrape").on("click", function (e) {
-  e.preventDefault();
-  $(this).text("Loading");
-  scrapeSpinner.toggleClass("d-none");
-
-  $.ajax({
-    type: "POST",
-    url: "/longtask",
-    success: function (data, status, request) {
-      status_url = request.getResponseHeader("Location");
-      updateProgress(status_url);
-    },
-    error: function () {
-      alert("Unexpected error");
-    },
+      },
+      error: function () {
+        alert("Unexpected error");
+      },
+    });
   });
 });
